e91a3d9a394ea70d3f44b1e80287c42d1be5a775
KritsanaDK <kritsana.dk@gmail.com>
2023-08-03 16:26:08 +0700

    .

---
+++ src/components/Contents.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import light from '../image/light.svg'
+import dark from '../image/dark.svg'
+
+const Contents = () => {
+    return (
+        <main>
+            <div>
+                <h1>Kritsana Ding Officeial</h1>
+                <p>Dark Mode Workshop</p>
+            </div>
+
+            <img src={light} alt="Logo" />
+
+        </main>
+    )
+}
+
+export default Contents
---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Title = () => {
+    return (
+        <header>
+            <span>
+                Mode
+            </span>
+
+        </header>
+    )
+}
+
+export default Title
